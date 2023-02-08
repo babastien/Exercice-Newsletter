ffe3805fd89b8b98fec608e30ded200bf0495eaf
babastien <ballestero.bastien@gmail.com>
2023-02-08 13:21:56 +0100

    fifth version

--- newsletter.sql
+++ newsletter.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : ven. 03 fév. 2023 à 09:20
+-- Généré le : mer. 08 fév. 2023 à 12:20
 -- Version du serveur : 8.0.30
 -- Version de PHP : 8.1.10
 
@@ -24,6 +24,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `interests`
+--
+
+CREATE TABLE `interests` (
+  `id` int NOT NULL,
+  `interest_label` varchar(255) COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `interests`
+--
+
+INSERT INTO `interests` (`id`, `interest_label`) VALUES
+(1, 'Peinture'),
+(2, 'Sculpture'),
+(3, 'Photographie'),
+(4, 'Art contemporain'),
+(5, 'Films'),
+(6, 'Art numérique'),
+(7, 'Installations');
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `origin`
 --
 
@@ -31,6 +55,15 @@ CREATE TABLE `origin` (
   `id` int NOT NULL,
   `origin_label` varchar(255) COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `origin`
+--
+
+INSERT INTO `origin` (`id`, `origin_label`) VALUES
+(1, 'Un ami m’en a parlé'),
+(2, 'Recherche sur internet'),
+(3, 'Publicité dans un magazine');
 
 -- --------------------------------------------------------
 
@@ -44,12 +77,30 @@ CREATE TABLE `subscribers` (
   `email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `firstname` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `lastname` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `origin_id` int NOT NULL
+  `origin_id` int DEFAULT NULL,
+  `interest_id` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `subscribers_interests`
+--
+
+CREATE TABLE `subscribers_interests` (
+  `subscribers_id` int NOT NULL,
+  `interests_id` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `interests`
+--
+ALTER TABLE `interests`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `origin`
@@ -62,17 +113,31 @@ ALTER TABLE `origin`
 --
 ALTER TABLE `subscribers`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_subscribers` (`origin_id`);
+  ADD KEY `fk_origin` (`origin_id`),
+  ADD KEY `fk_interest` (`interest_id`);
+
+--
+-- Index pour la table `subscribers_interests`
+--
+ALTER TABLE `subscribers_interests`
+  ADD KEY `fk_subscriber_id` (`subscribers_id`),
+  ADD KEY `fk_interest_id` (`interests_id`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
+-- AUTO_INCREMENT pour la table `interests`
+--
+ALTER TABLE `interests`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
 -- AUTO_INCREMENT pour la table `origin`
 --
 ALTER TABLE `origin`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pour la table `subscribers`
@@ -88,7 +153,15 @@ ALTER TABLE `subscribers`
 -- Contraintes pour la table `subscribers`
 --
 ALTER TABLE `subscribers`
-  ADD CONSTRAINT `fk_subscribers` FOREIGN KEY (`origin_id`) REFERENCES `origin` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD CONSTRAINT `fk_interest` FOREIGN KEY (`interest_id`) REFERENCES `interests` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `fk_origin` FOREIGN KEY (`origin_id`) REFERENCES `origin` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Contraintes pour la table `subscribers_interests`
+--
+ALTER TABLE `subscribers_interests`
+  ADD CONSTRAINT `fk_interest_id` FOREIGN KEY (`interests_id`) REFERENCES `interests` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `fk_subscriber_id` FOREIGN KEY (`subscribers_id`) REFERENCES `subscribers` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
